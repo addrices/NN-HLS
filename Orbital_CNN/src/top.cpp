de9d5a2142d6baec1246ec864a686b82892a694b
addrice <161220151@smail.nju.edu.cn>
2020-03-08 20:26:15 +0800

    temp

--- Orbital_CNN/src/top.cpp
+++ Orbital_CNN/src/top.cpp
@@ -1,3 +1,4 @@
+#define AP_INT_MAX_W 8192
 #include "../../nn-h/FcnnLayer.h"
 #include "../../nn-h/util.h"
 #include "../../nn-h/conv.h"
@@ -52,9 +53,9 @@
 //
 const ap_int<8*3*3> Weight1[1][16];
 const ap_int<8> Bias[32];
-void top(hls::stream<ap_uint<5*1*4> >& in,hls::stream<ap_uint<5*4*4> >& out){
-	const unsigned Batch = 5;
-	//ConvLayer_NOPAD_Orbital<Batch,C2_KSIZE,C2_SIZE,C2_INCHANNEL,C2_OUTCHANNEL,1,8,4,1,8,4,12>(in,out,C2_W,C2_B,C2_SCALEBIT,1);
+void top(hls::stream<ap_uint<8*1*4> >& in,hls::stream<ap_uint<8*8*4> >& out){
+	const unsigned Batch = 8;
+	ConvLayer_NOPAD_Orbital<Batch,C2_KSIZE,C2_SIZE,C2_INCHANNEL,C2_OUTCHANNEL,1,8,8,1,8,4,12>(in,out,C2_W,C2_B,C2_SCALEBIT,1);
 
 }
 
@@ -62,9 +63,9 @@ void top(hls::stream<ap_uint<5*1*4> >& in,hls::stream<ap_uint<5*4*4> >& out){
 //	//template<unsigned Depth,unsigned ASize,unsigned WSize,unsigned ABit,unsigned WBit,unsigned MBit>
 //	//void Orbital_Gemm(ap_uint<ABit*ASize*Depth> activation,ap_int<WBit*WSize*Depth> weight,ap_int<MBit*ASize*WSize> o){
 //	for(int i = 0;i < 100;i++){
-//		ap_uint<4*5*9> a = act.read();
-//		ap_int<8*8*9> b = wei.read();
-//		ap_int<12*5*8> c;
+//		ap_uint<4*20*20>  a = act.read();
+//		ap_int<8*20*20>  b = wei.read();
+//		ap_int<12*20*20> c;
 //		c = Orbital_Gemm<20,20,20,4,8,12>(a,b);
 //		res.write(c);
 //	}
@@ -72,284 +73,9 @@ void top(hls::stream<ap_uint<5*1*4> >& in,hls::stream<ap_uint<5*4*4> >& out){
 
 const ap_int<8*8> Weight2[(16/8)*3*3*(32/8)][8];
 
-//void top(hls::stream<ap_uint<8*4*1> >& in,hls::stream<ap_uint<8*4*1> >& out){
-//	Conv_MulAct_ScaleBit<3,16,32,1,8,4,12,28,8,8>(in,out,Weight2,Bias,7,1);
+//void top(hls::stream<ap_uint<16*4> >& in,hls::stream<ap_uint<32*4> >& out){
+//	C2:ConvLayer_NOPAD_ScaleBit<C2_KSIZE,WBIT,ABIT,C2_MBIT,C2_INCHANNEL,C2_OUTCHANNEL,C2_STRIDE,C2_SIZE,8,8>(in,out,C2_W_,C2_B,C2_SCALEBIT,8);
 //}
 
 
-//// Dot
-///*
-//void top(ap_int<IBit1*Ksize1*Ksize1*InChannel1>& in,ap_int<MBit1*OutChannel1>& out){
-//#pragma HLS INTERFACE ap_ovld both port=in
-//#pragma HLS INTERFACE ap_ovld both port=out
-//#pragma HLS DATAFLOW
-//	const int P = InChannel1*Ksize1*Ksize1;
-//	const int InBlockSize = IBit1*Ksize1*Ksize1*InChannel1;
-//	ap_int<KBit1*P> DotWeight[OutChannel1] = {2,4,13,23,45,12,3,4,5,23,15,16,23,32,25,14};
-//	ap_int<MBit1> OutSinglePix[OutChannel1];
-//	ap_int<IBit1*Ksize1*Ksize1*InChannel1> in1[OutChannel1];
-//	for(int l = 0; l < OutChannel1;l++){
-//#pragma HLS UNROLL
-//		in1[l] = in;
-//	}
-//	for(int l = 0; l < OutChannel1;l++){
-//#pragma HLS UNROLL
-//		OutSinglePix[l] = Dot<InChannel1*Ksize1*Ksize1,IBit1,KBit1,MBit1>(DotWeight[l],in1);
-//	}
-//
-//	for(int q = 0; q < OutChannel1;q++){
-//#pragma HLS UNROLL
-//		out((q+1)*MBit1-1,q*MBit1) = OutSinglePix[q];
-//	}
-//}*/
-//
-//
-//
-////#define GEMMSIZE 3
-//
-////void aTop(hls::stream<ap_int<8> > &in,hls::stream<ap_int<8> > &out);
-////using namespace hls;
-//
-///*
-//void Gemm_(ap_int<8> weight[GEMMSIZE][GEMMSIZE],ap_int<8> activation[GEMMSIZE][GEMMSIZE],ap_int<8> o[GEMMSIZE][GEMMSIZE]){
-//	ap_int<8> W[GEMMSIZE][GEMMSIZE],A[GEMMSIZE][GEMMSIZE],O[GEMMSIZE][GEMMSIZE];
-//	for(int a = 0; a < GEMMSIZE; a++){
-//#pragma HLS UNROLL
-//		for(int b = 0; b < GEMMSIZE; b++){
-//#pragma HLS UNROLL
-//			O[a][b] = 0;
-//			A[a][b] = activation[(a+b)%GEMMSIZE][b];
-//			W[a][b] = weight[a][(a+b)%GEMMSIZE];
-//		}
-//	}
-//	for(int i = 0;i < GEMMSIZE;i++){
-//#pragma HLS PIPELINE II = 1
-//		for(int j = 0; j < GEMMSIZE; j++){
-//#pragma HLS UNROLL
-//			for(int k = 0; k < GEMMSIZE;k++){
-//#pragma HLS UNROLL
-//				O[j][k] = O[j][k] + W[j][k] * A[j][k];
-//				W[j][k] = W[j][(k+1)%GEMMSIZE];
-//				A[j][k] = A[(j+1)%GEMMSIZE][j];
-//			}
-//		}
-//	}
-//	for(int c = 0; c < GEMMSIZE; c++){
-//#pragma HLS UNROLL
-//		for(int d = 0; d < GEMMSIZE; d++){
-//#pragma HLS UNROLL
-//			o[c][d] = O[c][d];
-//		}
-//	}
-//	return;
-//}
-//*/
-//
-//
-////ap_int<8> k1_8[9] = {1,2,3,4,5,6,7,8,9};
-////ap_int<8> k2[9] = {3,2,1,6,5,4,9,8,7};
-//ap_int<32> k1_32[9] = {1,2,3,4,5,6,7,8,9};
-//ap_int<32> k2_32[9] = {1,2,3,4,5,1,7,8,9};
-//ap_int<32> k3_32[9] = {1,2,3,4,5,4,7,8,9};
-//ap_int<32> k4_32[9] = {1,2,3,4,5,4,7,8,9};
-//double k1_d[9] = {1.2,2.3,3.4,4.5,5.6,6.7,7.8,8.9,9.0};
-//double k2_d[9] = {1.2,2.3,3.4,4.5,5.6,6.7,7.8,8.9,9.0};
-//double k3_d[9] = {1.2,2.3,3.4,4.5,5.6,6.7,7.8,8.9,9.0};
-//double k4_d[9] = {1.2,2.3,3.4,4.5,5.6,6.7,7.8,8.9,9.0};
-///*
-//void four2one(stream<ap_int<8> >& in1,stream<ap_int<8> >& in2,stream<ap_int<8> >& in3,stream<ap_int<8> >& in4,stream<ap_int<32> >& out){
-//	for(int j = 0;j < 48*64;j++){
-//		ap_int<8> m1 = in1.read();
-//		ap_int<8> m2 = in2.read();
-//		ap_int<8> m3 = in3.read();
-//		ap_int<8> m4 = in4.read();
-//		ap_int<32> outm = m1+(m2<<8)+(m3<<16)+(m4<<24);
-//		out.write(outm);
-//	}
-//}
-//
-//void one2four(stream<ap_int<8> >& out1,stream<ap_int<8> >& out2,stream<ap_int<8> >& out3,stream<ap_int<8> >& out4,stream<ap_int<32> >& in){
-//	for(int i = 0;i < 48*64;i++){
-//		ap_int<32> num = in.read();
-//		ap_int<8> n1 = num%256;
-//		ap_int<8> n2 = (num>>8)%256;
-//		ap_int<8> n3 = (num>>16)%256;
-//		ap_int<8> n4 = (num>>24)%256;
-//		out1.write(n1);
-//		out2.write(n2);
-//		out3.write(n3);){
-//		out4.write(n4);
-//	}
-//}*/
-//
-///*
-//void aTop(stream<ap_int<32> > &in,stream<ap_int<32> > &out){
-//#pragma HLS DATAFLOW
-//	stream<ap_int<8> > in1("in1");
-//	stream<ap_int<8> > in2("in2");
-//	stream<ap_int<8> > in3("in3");
-//	stream<ap_int<8> > in4("in4");
-//	stream<ap_int<8> > out1("out1");
-//	stream<ap_int<8> > out2("out2");
-//	stream<ap_int<8> > out3("out3");
-//	stream<ap_int<8> > out4("out4");
-//
-//	one2four(in1,in2,in3,in4,in);
-//	Conv_Top<3,1,8,8,8,8,8,48,64> (in1,out1,k1);
-//	Conv_Top<3,1,8,8,8,8,8,48,64> (in2,out2,k1);
-//	Conv_Top<3,1,8,8,8,8,8,48,64> (in3,out3,k1);
-//	Conv_Top<3,1,8,8,8,8,8,48,64> (in4,out4,k1);
-//	four2one(out1,out2,out3,out4,out);
-//
-//	return;
-//}
-//*/
-//
-///*
-//void top(hls::stream<double >& in,hls::stream<double > &out){
-//#pragma HLS DATAFLOW
-//
-//	hls::stream<ap_int<32> > temp1,temp2,temp3,temp4;
-//	hls::stream<ap_int<32> > temp5,temp7,temp6,temp8;
-//	hls::stream<ap_int<32> > tempo1,tempo2,tempo3,tempo4;
-//	StrExt4Str<32,3072>(in,temp5,temp6,temp7,temp8);
-//	Conv_N<3,1,32,32,32,32,48,64> (temp5,temp1,k1_32);
-//	Conv_N<3,1,32,32,32,32,48,64> (temp6,temp2,k2_32);
-//	Conv_N<3,1,32,32,32,32,48,64> (temp7,temp3,k3_32);
-//	Conv_N<3,1,32,32,32,32,48,64> (temp8,temp4,k4_32);
-//	MaxPooling_Str<2,32,46,62>(temp1,tempo1);
-//	MaxPooling_Str<2,32,46,62>(temp2,tempo2);
-//	MaxPooling_Str<2,32,46,62>(temp3,tempo3);		   	   	   	   	   	   	   	   	   	   	   	   	   	   	   {{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9}},\
-//
-//	MaxPooling_Str<2,32,46,62and MobileNet, with the right mix
-//of 1-, 2- and 3-bit parameters that average to just 1.4 bits can equal the accuracy
-//of homogeneous 2-bit versions of these networks. Further, we provide analyses
-//to show that the heterogeneously binarized systems yield FPGA- and ASIC-based
-//implementations that are correspondingly more efficient i>(temp4,tempo4);
-//	for(int i = 0;i < 46;i++){
-//		for(int j = 0;j < 62;j++){
-//			ap_int<32> T1 = tempo1.read();
-//			ap_int<32> T2 = tempo2.read();
-//			ap_int<32> T3 = tempo3.read();
-//			ap_int<32> T4 = tempo4.read();
-//			ap_int<32> T = T1+T2+T3+T4;
-//			out.write(T);
-//		}
-//	}
-//}*/
 
-/*
-void top(hls::stream<ap_int<32> >& in,hls::stream<ap_int<32> >& out){
-#pragma HLS DATAFLOW
-	//hls::stream<ap_int<32> > temp1;
-	Conv_N<3,1,32,32,32,32,48,64> (in,out,k1_32);
-	//MaxPooling_Str<2,32,46,62>(temp1,out);
-
-}
-*/
-
-
-/*
-void Gemm(ap_int<8> &w1,ap_int<8> &w2,ap_int<8> &w3,ap_int<8> &w4,ap_int<8> &w5,ap_int<8> &w6,ap_int<8> &w7,ap_int<8> &w8,ap_int<8> &w9,\
-		ap_int<8> &a1,ap_int<8> &a2,ap_int<8> &a3,ap_int<8> &a4,ap_int<8> &a5,ap_int<8> &a6,ap_int<8> &a7,ap_int<8> &a8,ap_int<8> &a9,\
-		ap_int<8> &o1,ap_int<8> &o2,ap_int<8> &o3,ap_int<8> &o4,ap_int<8> &o5,ap_int<8> &o6,ap_int<8> &o7,ap_int<8> &o8,ap_int<8> &o9){
-#pragma HLS INTERFACE ap_ovld port=w1
-#pragma HLS INTERFACE ap_ovld port=a1
-#pragma HLS INTERFACE ap_ovld port=o1
-#pragma HLS INTERFACE ap_ovld port=w2
-#pragma HLS INTERFACE ap_ovld port=a2
-#pragma HLS INTERFACE ap_ovld port=o2
-#pragma HLS INTERFACE ap_ovld port=w3
-#pragma HLS INTERFACE ap_ovld port=a3
-#pragma HLS INTERFACE ap_ovld port=o3
-#pragma HLS INTERFACE ap_ovld port=w4
-#pragma HLS INTERFACE ap_ovld port=a4
-#pragma HLS INTERFACE ap_ovld port=o4
-#pragma HLS INTERFACE ap_ovld port=w5
-#pragma HLS INTERFACE ap_ovld port=a5
-#pragma HLS INTERFACE ap_ovld port=o5
-#pragma HLS INTERFACE ap_ovld port=w6
-#pragma HLS INTERFACE ap_ovld port=a6
-#pragma HLS INTERFACE ap_ovld port=o6
-#pragma HLS INTERFACE ap_ovld port=w7
-#pragma HLS INTERFACE ap_ovld port=a7
-#pragma HLS INTERFACE ap_ovld port=o7
-#pragma HLS INTERFACE ap_ovld port=w8
-#pragma HLS INTERFACE ap_ovld port=a8
-#pragma HLS INTERFACE ap_ovld port=o8
-#pragma HLS INTERFACE ap_ovld port=w9
-#pragma HLS INTERFACE ap_ovld port=a9
-#pragma HLS INTERFACE ap_ovld port=o9
-	ap_int<8> weight[GEMMSIZE][GEMMSIZE],activation[GEMMSIZE][GEMMSIZE],out[GEMMSIZE][GEMMSIZE];
-	weight[0][0] = w1; activation[0][0] = a1;
-	weight[0][1] = w2; activation[0][1] = a2;
-	weight[0][2] = w3; activation[0][2] = a3;
-	weight[1][0] = w4; activation[1][0] = a4;
-	weight[1][1] = w5; activation[1][1] = a5;
-	weight[1][2] = w6; activation[1][2] = a6;
-	weight[2][0] = w7; activation[2][0] = a7;
-	weight[2][1] = w8; activation[2][1] = a8;
-	weight[2][2] = w9; activation[2][2] = a9;
-	Gemm_(weight,activation,out);
-	o1 = out[0][0];o2 = out[0][1]; o3 = out[0][2];
-	o4 = out[1][0];o5 = out[1][1]; o6 = out[1][2];
-	o7 = out[2][0];o8 = out[2][1]; o9 = out[2][2];
-	return;
-}
-
-void conv_top(int *img,int *ker,int *ret){
-//#pragma HLS INTERFACE s_axilite port=h bundle=control
-//#pragma HLS INTERFACE s_axilite port=w bundle=control
-#pragma HLS INTERFACE m_axi offset=slave port=img bundle=hostmem depth=256
-#pragma HLS INTERFACE s_axilite port=img bundle=control
-#pragma HLS INTERFACE m_axi offset=slave port=ker bundle=hostmem depth=9*9
-#pragma HLS INTERFACE s_axilite port=ker bundle=control
-#pragma HLS INTERFACE m_axi offset=slave port=ret bundle=hostmem depth=256*9
-#pragma HLS INTERFACE s_axilite port=ret bundle=control
-#pragma HLS INTERFACE s_axilite port=return bundle=control
-
-
-	static int buffer[16][16];
-#pragma HLS ARRAY_PARTITION variable=buffer complete dim=1
-	static int conv_kernel[9][9];			//batch = 9
-#pragma HLS ARRAY_PARTITION variable=conv_kernel complete dim=0
-
-	memcpy(buffer, img, 16*16 * sizeof(int));
-	memcpy(conv_kernel, img, 9*9 * sizeof(int));
-
-	int conv;
-	int R[9][16][16];
-
-	int GEMM_A[9][9];
-	int OUT[9][9];
-
-	for(int i = 0; i < 16; i++){
-		for(int j = 0; j < 16; j+=8){
-			for(int k = 0; k < 9;k++){
-				for(int m = 0; m < 3;m++){
-					for(int n = 0; n < 3;n++){
-						if(i+m-1<0 || i+m-1>15||j+k+n-1<0||j+k+n-1>15||k==8)
-							GEMM_A[3*m+n][k] = 0;
-						else
-							GEMM_A[3*m+n][k] = buffer[i+m-1][j+k+n-1];
-					}
-				}
-			}
-			Gemm(conv_kernel,GEMM_A,OUT);
-			for(int o1 = 0; o1 < 9; o1++){
-				for(int o2 = 0; o2 < 8; o2++){
-					R[o1][i][j+o2] = OUT[o1][o2];
-				}
-			}
-		}
-	}
-	for(int e=0;e < 9;e++){
-		for(int r=0;r < 16;r++){
-			for(int t=0;t < 9;t++){
-				ret[e*16*16+r*16+t] = R[e][r][t];
-			}
-		}
-	}
-	return;
-}
-*/
--- nn-h/conv_systolic.h
+++ nn-h/conv_systolic.h
@@ -160,11 +160,11 @@ ap_int<MBit*ASize*WSize> Orbital_Gemm(ap_uint<ABit*ASize*Depth> activation,ap_in
 		}
 	}
 	ap_int<MBit*ASize*WSize> o;
-	for(unsigned c = 0; c < ASize; c++){
+	for(unsigned c = 0; c < WSize; c++){
 #pragma HLS UNROLL
-		for(unsigned d = 0; d < WSize; d++){
+		for(unsigned d = 0; d < ASize; d++){
 #pragma HLS UNROLL
-			o((c*WSize+d+1)*MBit-1,(c*WSize+d)*MBit) = O[c][d];
+			o((c*ASize+d+1)*MBit-1,(c*ASize+d)*MBit) = O[d][c];
 //			cout << O[c][d] << " ";
 		}
 //		cout << endl;
@@ -223,17 +223,21 @@ void Conv_MulAct_Oribital(hls::stream<ap_uint<Batch*ABit*InP> >& in,hls::stream<
 	const unsigned MidPack = OutChannel / MidP;
 	const unsigned OutPack = OutChannel / OutP;
 	ap_uint<ABit> InArray[InP][Batch][Depth];
-	ap_int<MBit> MidArray[Batch][OutChannel];
-	ap_uint<ABit> OutArray[Batch][OutChannel];
+	ap_int<MBit> MidArray[Batch*OutChannel];
+	ap_uint<ABit> OutArray[Batch*OutChannel];
+#pragma HLS array_partition variable=InArray complete
+#pragma HLS array_partition variable=MidArray complete
+#pragma HLS array_partition variable=OutArray complete
+
 	const unsigned Nums = ((Size-KSize)/Stride+1)*((Size-KSize)/Stride+1);
 //reps
 	LoopRep:for(unsigned rep = 0;rep < reps;rep++){
 		LoopPix:for(unsigned i = 0;i < Nums;i++){
-			for(unsigned batch = 0;batch < Batch;batch++){
+			for(unsigned ochan = 0;ochan < OutChannel;ochan++){
 #pragma HLS UNROLL
-				for(unsigned ochan = 0;ochan < OutChannel;ochan++){
+				for(unsigned batch = 0;batch < Batch;batch++){
 #pragma HLS UNROLL
-					MidArray[batch][ochan] = Bias[ochan];
+					MidArray[ochan*Batch+batch] = Bias[ochan];
 				}
 			}
 			LoopInPack:for(unsigned ipack = 0;ipack < InPack;ipack++){
@@ -248,88 +252,58 @@ void Conv_MulAct_Oribital(hls::stream<ap_uint<Batch*ABit*InP> >& in,hls::stream<
 						}
 					}
 				}
-				for(int a = 0; a < 3;a++){
-					for(int b = 0;b < 3;b++){
-						for(int c = 0;c < Batch;c++){
-							// cout << InArray[0][c][a*3+b] << " ";
-						}
-					}
-					// cout << endl;
-				}
 				for(unsigned mpack = 0;mpack < MidPack;mpack++){
 					Orbital:for(unsigned inp = 0;inp < InP;inp++){
+// #pragma HLS PIPELINE II = Depth+2
 						//one orbital
 						unsigned NInChannel = ipack * InP + inp;
-						ap_uint<ABit> act[Batch][Depth];
-						ap_int<WBit> wei[MidP][Depth];
-						ap_int<MBit> res[Batch][MidP];
-#pragma HLS array_partition variable=act complete
-#pragma HLS array_partition variable=wei complete
-#pragma HLS array_partition variable=res complete
+						ap_uint<ABit*Batch*Depth> act;
+						ap_int<WBit*MidP*Depth> wei;
+						ap_int<MBit*Batch*MidP> res;
 						//read
 						for(unsigned batch = 0;batch < Batch;batch++){
 #pragma HLS UNROLL
 							for(unsigned depth = 0;depth < Depth;depth++){
 #pragma HLS UNROLL
-								act[batch][depth] = InArray[inp][batch][(batch+depth)%Depth];
+								act((batch*Depth+depth+1)*ABit-1,(batch*Depth+depth)*ABit) = InArray[inp][batch][depth];
 								// cout << act[batch][depth] << " ";
 							}
 							// cout << endl;
 						}
-						
+//						act(3,0) = InArray[inp][0][0];	        act(7,4) = InArray[inp][0][1];act(11,8) = InArray[inp][0][2]; act(15,12) = InArray[inp][0][3]; act(19,16) = InArray[inp][0][4];	act(23,20) = InArray[inp][0][5];    act(27,24) = InArray[inp][0][6];   act(31,28) = InArray[inp][0][7];  act(35,32) = InArray[inp][0][8];
+//						act(3+36,0+36) = InArray[inp][1][0];	act(7+36,4+36) = InArray[inp][1][1];act(11+36,8+36) = InArray[inp][1][2]; act(15+36,12+36) = InArray[inp][1][3]; act(19+36,16+36) = InArray[inp][1][4];	act(23+36,20+36) = InArray[inp][1][5];    act(27+36,24+36) = InArray[inp][1][6];   act(31+36,28+36) = InArray[inp][1][7];  act(35+36,32+36) = InArray[inp][1][8];
+//						act(3+72,0+72) = InArray[inp][2][0];	act(7+72,4+72) = InArray[inp][2][1];act(11+72,8+72) = InArray[inp][2][2]; act(15+72,12+72) = InArray[inp][2][3]; act(19+72,16+72) = InArray[inp][2][4];	act(23+72,20+72) = InArray[inp][2][5];    act(27+72,24+72) = InArray[inp][2][6];   act(31+72,28+72) = InArray[inp][2][7];  act(35+72,32+72) = InArray[inp][2][8];
+//						act(3+108,0+108) = InArray[inp][3][0];	act(7+108,4+108) = InArray[inp][3][1];act(11+108,8+108) = InArray[inp][3][2]; act(15+108,12+108) = InArray[inp][3][3]; act(19+108,16+108) = InArray[inp][3][4];	act(23+108,20+108) = InArray[inp][3][5];    act(27+108,24+108) = InArray[inp][3][6];   act(31+108,28+108) = InArray[inp][3][7];  act(35+108,32+108) = InArray[inp][3][8];
+//						act(3+144,0+144) = InArray[inp][4][0];	act(7+144,4+144) = InArray[inp][4][1];act(11+144,8+144) = InArray[inp][4][2]; act(15+144,12+144) = InArray[inp][4][3]; act(19+144,16+144) = InArray[inp][4][4];	act(23+144,20+144) = InArray[inp][4][5];    act(27+144,24+144) = InArray[inp][4][6];   act(31+144,28+144) = InArray[inp][4][7];  act(35+144,32+144) = InArray[inp][4][8];
+//						act(3+180,0+180) = InArray[inp][5][0];	act(7+180,4+180) = InArray[inp][5][1];act(11+180,8+180) = InArray[inp][5][2]; act(15+180,12+180) = InArray[inp][5][3]; act(19+180,16+180) = InArray[inp][5][4];	act(23+180,20+180) = InArray[inp][5][5];    act(27+180,24+180) = InArray[inp][5][6];   act(31+180,28+180) = InArray[inp][5][7];  act(35+180,32+180) = InArray[inp][5][8];
+//						act(3+216,0+216) = InArray[inp][6][0];	act(7+216,4+216) = InArray[inp][6][1];act(11+216,8+216) = InArray[inp][6][2]; act(15+216,12+216) = InArray[inp][6][3]; act(19+216,16+216) = InArray[inp][6][4];	act(23+216,20+216) = InArray[inp][6][5];    act(27+216,24+216) = InArray[inp][6][6];   act(31+216,28+216) = InArray[inp][6][7];  act(35+216,32+216) = InArray[inp][6][8];
+//						act(3+252,0+252) = InArray[inp][7][0];	act(7+252,4+252) = InArray[inp][7][1];act(11+252,8+252) = InArray[inp][7][2]; act(15+252,12+252) = InArray[inp][7][3]; act(19+252,16+252) = InArray[inp][7][4];	act(23+252,20+252) = InArray[inp][7][5];    act(27+252,24+252) = InArray[inp][7][6];   act(31+252,28+252) = InArray[inp][7][7];  act(35+252,32+252) = InArray[inp][7][8];
+
+
 						for(unsigned midp = 0;midp < MidP;midp++){
 #pragma HLS UNROLL
 							unsigned NOutChannel = mpack * MidP + midp;
-							for(unsigned depth = 0;depth < Depth;depth++){
-#pragma HLS UNROLL
-								wei[midp][depth] = Weight[NOutChannel][NInChannel](((midp+depth)%Depth+1)*WBit-1,((midp+depth)%Depth)*WBit);
-								// cout << wei[midp][depth] << " ";
-							}
-							// cout << endl;
+							wei((midp+1)*Depth*WBit-1,midp*Depth*WBit) = Weight[NOutChannel][NInChannel];
 						}
-
-						for(unsigned i = 0;i < Batch;i++)
-#pragma HLS UNROLL
-							for(unsigned j = 0;j < MidP;j++)
-#pragma HLS UNROLL
-								res[i][j] = 0;
+//						wei(71,0) = Weight[mpack*MidP][NInChannel];
+//						wei(143,72) = Weight[mpack*MidP+1][NInChannel];
+//						wei(215,144) = Weight[mpack*MidP+2][NInChannel];
+//						wei(287,216) = Weight[mpack*MidP+3][NInChannel];
+//						wei(359,288) = Weight[mpack*MidP+4][NInChannel];
+//						wei(431,360) = Weight[mpack*MidP+5][NInChannel];
+//						wei(503,432) = Weight[mpack*MidP+6][NInChannel];
+//						wei(575,504) = Weight[mpack*MidP+7][NInChannel];
 
 						//calculate
-						for(unsigned i = 0;i < Depth;i++){
-#pragma HLS PIPELINE II = 1
-							for(unsigned j = 0; j < Batch; j++){
-#pragma HLS UNROLL
-								for(unsigned k = 0; k < MidP;k++){
-#pragma HLS UNROLL
-									res[j][k] = res[j][k] + wei[k][j] * act[j][k];
-								}
-							}
-							for(unsigned j = 0;j < Batch;j++){
-#pragma HLS UNROLL
-								ap_uint<ABit> ATemp = act[j][0];
-								for(unsigned d = 0;d < Depth-1;d++){
-#pragma HLS UNROLL
-									act[j][d] = act[j][d+1];
-								}
-								act[j][Depth-1] = ATemp;
-							}
-							for(unsigned k = 0;k < MidP;k++){
-#pragma HLS UNROLL
-								ap_int<WBit> WTemp = wei[k][0];
-								for(unsigned d = 0;d < Depth-1;d++){
-#pragma HLS UNROLL
-									wei[k][d] = wei[k][d+1];
-								}
-								wei[k][Depth-1] = WTemp;
-							}
-						}
+						res = Orbital_Gemm<Depth,Batch,MidP,ABit,WBit,MBit>(act,wei);
 						//write
-						for(unsigned j = 0; j < Batch; j++){
+						for(unsigned j = 0; j < MidP; j++){
 #pragma HLS UNROLL
-							for(unsigned k = 0; k < MidP;k++){
+							for(unsigned k = 0; k < Batch;k++){
 #pragma HLS UNROLL
-								unsigned OChannel = mpack*MidP+k;
-								MidArray[j][OChannel] += res[j][k];
+								unsigned OChannel = mpack*MidP+j;
+								ap_int<MBit> R = res((j*Batch+k+1)*MBit-1,(j*Batch+k)*MBit);
+								MidArray[OChannel*Batch+k] += R;
 								// cout << " res " << res[j][k] << " Mid " << MidArray[j][OChannel];
 							}
 							// cout << endl;
@@ -343,9 +317,9 @@ void Conv_MulAct_Oribital(hls::stream<ap_uint<Batch*ABit*InP> >& in,hls::stream<
 							for(unsigned batch = 0;batch < Batch;batch++){
 								unsigned NChannel = opack*OutP + outp;
 								unsigned OToffset = outp*Batch + batch;
-								OutArray[batch][NChannel] = SCALE_RELU_ScaleBit<ABit,MBit>(MidArray[batch][NChannel],Scale);
+								OutArray[NChannel*Batch+batch] = SCALE_RELU_ScaleBit<ABit,MBit>(MidArray[NChannel*Batch+batch],Scale);
 								// cout << OutArray[batch][NChannel] << endl;
-								OutTemp((OToffset+1)*ABit-1,OToffset*ABit) = OutArray[batch][NChannel];
+								OutTemp((OToffset+1)*ABit-1,OToffset*ABit) = OutArray[NChannel*Batch+batch];
 							}
 						}
 						out.write(OutTemp);
